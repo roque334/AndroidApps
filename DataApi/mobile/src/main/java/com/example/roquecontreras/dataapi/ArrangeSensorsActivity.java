39cc64e06647e5468b7285a336a76df6c80fbed2
roque334 <roque334@hotmail.com>
2016-01-22 18:10:26 -0500

    Optimización de imports

--- DataApi/mobile/src/main/java/com/example/roquecontreras/dataapi/ArrangeSensorsActivity.java
+++ DataApi/mobile/src/main/java/com/example/roquecontreras/dataapi/ArrangeSensorsActivity.java
@@ -1,10 +1,10 @@
 package com.example.roquecontreras.dataapi;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
-import android.app.Activity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
