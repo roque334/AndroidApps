79a0372ca92556e3bdc40154f1d3992c36a61f4f
roque334 <roque334@hotmail.com>
2016-01-22 18:14:28 -0500

    Se agrega toda la lógica de generar el arreglo de bytes a enviar en partes múltiples

--- DataApi/mobile/src/main/java/com/example/roquecontreras/dataapi/MultipartFileRequest.java
+++ DataApi/mobile/src/main/java/com/example/roquecontreras/dataapi/MultipartFileRequest.java
@@ -1,5 +1,6 @@
 package com.example.roquecontreras.dataapi;
 
+import android.content.Context;
 import android.util.Log;
 
 import com.android.volley.AuthFailureError;
@@ -10,8 +11,16 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.HttpHeaderParser;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -23,9 +32,29 @@ class MultipartFileRequest extends Request<NetworkResponse> {
     private final Map<String, String> mHeaders;
     private final String mMimeType;
     private final byte[] mMultipartBody;
+    private final Context mContext;
+    private File mfile;
 
-    public MultipartFileRequest(String url, Map<String, String> headers, String mimeType, byte[] multipartBody, Response.Listener<NetworkResponse> listener, Response.ErrorListener errorListener) {
+    public MultipartFileRequest(String url, File file, Context context, Response.Listener<NetworkResponse> listener, Response.ErrorListener errorListener) {
         super(Method.POST, url, errorListener);
+        ByteArrayOutputStream bos;
+        DataOutputStream dos;
+        String mimeType;
+        byte[] multipartBody;
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "text/plain");
+        headers.put("Content-Disposition", "attachment;filename=" + file.getName());
+        headers.put("Accept", "application/json");
+        mimeType = "attachment;filename=" + file.getName();
+        bos = new ByteArrayOutputStream();
+        dos = new DataOutputStream(bos);
+
+        Log.d("Request", "FileSize = " + file.length());
+        buildTextPart(dos, readFromFile(file));
+        multipartBody = bos.toByteArray();
+
+        this.mfile = file;
+        this.mContext = context;
         this.mListener = listener;
         this.mErrorListener = errorListener;
         this.mHeaders = headers;
@@ -61,6 +90,14 @@ class MultipartFileRequest extends Request<NetworkResponse> {
     protected Response<NetworkResponse> parseNetworkResponse(NetworkResponse response) {
         try {
             deliverResponse(response);
+            try {
+                JSONObject jsonObject = new JSONObject(new String(response.data));
+                if (mfile.length() == jsonObject.getLong("file-size")) {
+                    mContext.deleteFile(mfile.getName());
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
             return Response.success(
                     response,
                     HttpHeaderParser.parseCacheHeaders(response));
@@ -78,5 +115,37 @@ class MultipartFileRequest extends Request<NetworkResponse> {
     @Override
     public void deliverError(VolleyError error) {
         mErrorListener.onErrorResponse(error);
+    }
+
+    private void buildTextPart(DataOutputStream dataOutputStream, String parameterValue) {
+        try {
+            dataOutputStream.writeBytes(parameterValue);
+        } catch (IOException e) {
+            Log.d("Request", "buildTextPart: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    private String readFromFile(File file) {
+        String contents;
+        FileInputStream fis;
+        byte[] bytes;
+        int length;
+
+        fis = null;
+        length = (int) file.length();
+        bytes = new byte[length];
+
+        try {
+            fis = new FileInputStream(file);
+            fis.read(bytes);
+            fis.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        contents = new String(bytes);
+        return contents;
     }
 }
