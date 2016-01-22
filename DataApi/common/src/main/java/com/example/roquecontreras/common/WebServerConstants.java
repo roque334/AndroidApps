4ae0b49b034419296f05735ce5eda1e08ffd0daa
roque334 <roque334@hotmail.com>
2016-01-22 18:09:22 -0500

    Se agrega variable estática que representa el label para el IdToken

--- DataApi/common/src/main/java/com/example/roquecontreras/common/WebServerConstants.java
+++ DataApi/common/src/main/java/com/example/roquecontreras/common/WebServerConstants.java
@@ -5,6 +5,8 @@ package com.example.roquecontreras.common;
  */
 public class WebServerConstants {
 
+    public static final String ID_TOKEN_LABEL = "id-token";
+
     public static final String WEB_SERVER_CLIENT_ID = "503008496668-e83akin5nrof707ijj8j7vs8edo4j5vb.apps.googleusercontent.com";
 
     public static final String LOCAL_WEB_SERVER_UPLOAD_MEASURE_FILE_URL = "http://10.0.2.2:8000/measureUpload";
