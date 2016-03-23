0ceef21ab08b155414e19af37ef3f0b7f845ceb8
roque334 <roque334@hotmail.com>
2016-03-23 16:01:21 -0500

    Se agregó un nuevo atributo que representa el tipo de muestra, por ejemplo RAW_ACCELERATION, TOTAL_ACCELERATION, BODY_ACCELERATION, entre otros.

--- DataApi/common/src/main/java/com/example/roquecontreras/common/Measurement.java
+++ DataApi/common/src/main/java/com/example/roquecontreras/common/Measurement.java
@@ -4,23 +4,34 @@ package com.example.roquecontreras.common;
  * Created by roquecontreras on 27/10/15.
  */
 public class Measurement {
-    float x;
-    float y;
-    float z;
+    private MeasureType type;
+    private float x;
+    private float y;
+    private float z;
     long timestamp;
 
-    public Measurement(float x, float y, float z, long timestamp) {
+    public Measurement(MeasureType type, float x, float y, float z, long timestamp) {
+        this.type = type;
         this.x = x;
         this.y = y;
         this.z = z;
         this.timestamp = timestamp;
     }
 
-    public Measurement(float[] measures, long timestamp) {
+    public Measurement(MeasureType type, float[] measures, long timestamp) {
+        this.type = type;
         this.x = measures[0];
         this.y = measures[1];
         this.z = measures[2];
         this.timestamp = timestamp;
+    }
+
+    public MeasureType getType() {
+        return type;
+    }
+
+    public void setType(MeasureType type) {
+        this.type = type;
     }
 
     public float getX() {
@@ -57,6 +68,6 @@ public class Measurement {
 
     @Override
     public String toString() {
-        return x + ";" + y + ";" + z + ";" + timestamp;
+        return type.toString() + ";" + x + ";" + y + ";" + z + ";" + timestamp + "#" + System.lineSeparator();
     }
 }
