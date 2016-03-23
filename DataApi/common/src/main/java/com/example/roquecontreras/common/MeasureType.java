c02a82a4a5de25f18978667749a8f7aa8d410e10
roque334 <roque334@hotmail.com>
2016-03-23 16:17:09 -0500

    Se creó un enumerable para representar los tipos de muestras que la aplicación registra.

---
+++ DataApi/common/src/main/java/com/example/roquecontreras/common/MeasureType.java
@@ -0,0 +1,24 @@
+package com.example.roquecontreras.common;
+
+/**
+ * Created by roquecontreras on 14/03/16.
+ */
+public enum MeasureType {
+    RAW_ACCELERATION(0), TOTAL_ACCELERATION(1), BODY_ACCELERATION(2), BODY_JERK(3), BODY_ACC_MAGNITUDE(4)
+    , RAW_ANGULAR_SPEED(5), ANGULAR_SPEED(6), ANGULAR_ACCELERATION(7), ANGULAR_SPEED_MAGNITUDE(8);
+
+    private int value;
+
+    private MeasureType(int val){
+        value = val;
+    }
+
+    public int getValue(){
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(this.getValue());
+    }
+}
