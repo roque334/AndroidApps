a8b07fdc45b2e4c13beab6995cc5d7660e34e2d0
roque334 <roque334@hotmail.com>
2016-03-23 15:58:45 -0500

    Se agregó el filtro de la componenete de la gravedad del acelerómetro. Se agregó el filtro incremental de la componente DC del giroscopio. Se corrigieron errores del filtro de la mediana. Se documentaron las funciones.

--- DataApi/common/src/main/java/com/example/roquecontreras/common/DSPFilter.java
+++ DataApi/common/src/main/java/com/example/roquecontreras/common/DSPFilter.java
@@ -7,26 +7,41 @@ import java.util.Arrays;
  */
 public class DSPFilter {
 
-    private static final float mDCBias = 0;
     private static float[][] accelerometerMedianWindow;
-    private static float[][] gyroscopeMedianWindow;
+    private static float[] mGravity;
+    private static final float mAlpha = 0.8f;
 
+    private static float[][] gyroscopeMedianWindow;
+    private static float[] mDCBias;
+    private static float mDCBiasCounter;
+
+    /**
+     * Initializes the windows to do the filtering process.
+     * @param x the value of the X axis.
+     * @param y the value of the Y axis.
+     * @param z the value of the Z axis.
+     * @param isAccelerometerValue indicates if the values are fom the accelerometer sensor.
+     */
     public static void initFilter(float x, float y, float z, boolean isAccelerometerValue) {
         if (accelerometerMedianWindow == null) {
             accelerometerMedianWindow = new float[3][];
+            mGravity = new float[3];
             accelerometerMedianWindow[0] = new float[3];
             accelerometerMedianWindow[1] = new float[3];
             accelerometerMedianWindow[2] = new float[3];
             Arrays.fill(accelerometerMedianWindow[0],0f);
             Arrays.fill(accelerometerMedianWindow[1],0f);
             Arrays.fill(accelerometerMedianWindow[2],0f);
+            Arrays.fill(mGravity,0f);
         }
 
         if (gyroscopeMedianWindow == null) {
+            mDCBias = new float[3];
             gyroscopeMedianWindow = new float[3][];
             gyroscopeMedianWindow[0] = new float[3];
             gyroscopeMedianWindow[1] = new float[3];
             gyroscopeMedianWindow[2] = new float[3];
+            mDCBiasCounter = 0;
             Arrays.fill(gyroscopeMedianWindow[0],0f);
             Arrays.fill(gyroscopeMedianWindow[1],0f);
             Arrays.fill(gyroscopeMedianWindow[2],0f);
@@ -43,6 +58,57 @@ public class DSPFilter {
         }
     }
 
+    /**
+     * Filtes the gravity component of the accelerometer values.
+     * @param x the value of the X axis.
+     * @param y the value of the Y axis.
+     * @param z the value of the Z axis.
+     * @return the body acceleration.
+     */
+    public static float[] gravitylFiltering(float x, float y, float z) {
+        float[] result;
+        result = new float[3];
+        mGravity[0] = mAlpha * mGravity[0] + (1 - mAlpha) * x;
+        mGravity[1] = mAlpha * mGravity[1] + (1 - mAlpha) * y;
+        mGravity[2] = mAlpha * mGravity[2] + (1 - mAlpha) * z;
+
+        result[0] = x - mGravity[0];
+        result[1] = y - mGravity[1];
+        result[2] = z - mGravity[2];
+
+        return result;
+    }
+
+    /**
+     * Filters the gyroscope values.
+     * @param x the value of the X axis.
+     * @param y the value of the Y axis.
+     * @param z the value of the Z axis.
+     * @return the angular speed.
+     */
+    public static float[] gyroscopeFiltering(float x, float y, float z) {
+        float[] result, aux;
+        result = new float[3];
+        aux = new float[3];
+        aux = smoothingAndFiltering(x, y, z, false);
+        mDCBiasCounter += 1;
+        mDCBias[0] = ((mDCBias[0] * (mDCBiasCounter-1)) + aux[0])/mDCBiasCounter;
+        mDCBias[1] = ((mDCBias[1] * (mDCBiasCounter-1)) + aux[1])/mDCBiasCounter;
+        mDCBias[2] = ((mDCBias[2] * (mDCBiasCounter-1)) + aux[2])/mDCBiasCounter;
+        result[0] = aux[0] - mDCBias[0];
+        result[1] = aux[1] - mDCBias[1];
+        result[2] = aux[2] - mDCBias[2];
+        return result;
+    }
+
+    /**
+     * Smooths and Filters the sensor values.
+     * @param x the value of the X axis.
+     * @param y the value of the Y axis.
+     * @param z the value of the Z axis.
+     * @param isAccelerometerValue indicates if the values are fom the accelerometer sensor.
+     * @return the filtered values of the sensor.
+     */
     public static float[] smoothingAndFiltering(float x, float y, float z, boolean isAccelerometerValue) {
         float[] result;
         result = new float[3];
@@ -60,19 +126,37 @@ public class DSPFilter {
             result[0] = thirdOrderButterWorthFilter(thirdOrderMedianFilter(gyroscopeMedianWindow[0]));
             result[1] = thirdOrderButterWorthFilter(thirdOrderMedianFilter(gyroscopeMedianWindow[1]));
             result[2] = thirdOrderButterWorthFilter(thirdOrderMedianFilter(gyroscopeMedianWindow[2]));
+
         }
         return result;
     }
 
+    /**
+     * Applies a third order media filter with a window of size three.
+     * @param window the window.
+     * @return the value filtered.
+     */
     private static float thirdOrderMedianFilter(float[] window){
+        float[] auxilarWindow = new float[3];
         float result;
-        Arrays.sort(window);
-        result = window[1];
+        for(int i=0;i<window.length;i++) {
+            auxilarWindow[i] = window[i];
+        }
         window[0] = window[1];
         window[1] = window[2];
+
+        Arrays.sort(auxilarWindow);
+
+        result = auxilarWindow[1];
+
         return result;
     }
 
+    /**
+     * Applies a a pre-build butter worth filter over a values.
+     * @param x the value to filter.
+     * @return the value filtered.
+     */
     private static float thirdOrderButterWorthFilter(float x){
         float num,dem,result;
         num = 0.52762f * ((float) Math.pow(x,3)) + 1.58287f * ((float) Math.pow(x,2)) + 1.58287f * x + 0.52762f;
@@ -81,10 +165,11 @@ public class DSPFilter {
         return result;
     }
 
-    private static float DCBiasFilter(float x) {
-        return x - mDCBias;
-    }
-
+    /**
+     * Calculates the DC component from a signal.
+     * @param x the signal.
+     * @return the DC component.
+     */
     private static float DCBiasFilter(float[] x){
         float result = 0;
         for (int i = 0; i < x.length; i++) {
