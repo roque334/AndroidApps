8e09aaaff06cb8714041668b9daa89dc53f517d9
roque334 <roque334@hotmail.com>
2016-04-15 18:55:02 -0500

    Código para la detección de una red

---
+++ DataApi/mobile/src/main/java/com/example/roquecontreras/dataapi/NetworkAndInternetConnection.java
@@ -0,0 +1,109 @@
+package com.example.roquecontreras.dataapi;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.preference.PreferenceManager;
+import android.support.v4.content.IntentCompat;
+
+import com.example.roquecontreras.common.MobileWearConstants;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * Created by roquecontreras on 24/03/16.
+ */
+public class NetworkAndInternetConnection {
+
+    private static ProgressDialog mProgressDialog;
+
+    public static void checkNetworkAndInternet(Context context) {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(context);
+            mProgressDialog.setTitle(context.getString(R.string.no_internet_connection_title));
+            mProgressDialog.setMessage(context.getString(R.string.no_internet_connection_message));
+        }
+        if (isMobileWiFiNetworkAvailable(context) && isInternetConnectionAvailable(1000)) {
+            mProgressDialog.setCancelable(true);
+            mProgressDialog.dismiss();
+        } else {
+            mProgressDialog.setIndeterminate(false);
+            mProgressDialog.setCancelable(false);
+            mProgressDialog.show();
+            resetAppAfterSomeTime(context);
+        }
+    }
+
+    private static void resetAppAfterSomeTime(final Context context) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Intent intentSignInActivity;
+                try {
+                    Thread.sleep(10000);
+                    if ((!isMobileWiFiNetworkAvailable(context)) && (!isInternetConnectionAvailable(1000))) {
+                        mProgressDialog.dismiss();
+                        intentSignInActivity = new Intent(context, SignInActivity.class);
+                        intentSignInActivity.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK | IntentCompat.FLAG_ACTIVITY_CLEAR_TASK);
+                        context.startActivity(intentSignInActivity);
+                    }
+                } catch (Exception e) {
+
+                }
+            }
+        }).start();
+    }
+
+    private static boolean isMobileWiFiNetworkAvailable(Context context) {
+        ConnectivityManager connectivityManager;
+        NetworkInfo activeNetworkInfo;
+        SharedPreferences sp;
+        boolean result = true;
+        connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        if (activeNetworkInfo != null && activeNetworkInfo.isConnected()) {
+            // connected to the internet
+            sp = PreferenceManager.getDefaultSharedPreferences(context);
+            if (sp.getBoolean(MobileWearConstants.KEY_HANDHELD_WEAR_SYNC_WIFI, true) && (activeNetworkInfo.getType() != ConnectivityManager.TYPE_WIFI)) {
+                //not connected to wifi
+                result = false;
+            }
+        } else {
+            // not connected to the internet
+            result = false;
+        }
+        return result;
+    }
+
+    private static boolean isInternetConnectionAvailable(int timeOut) {
+        InetAddress inetAddress = null;
+        try {
+            Future<InetAddress> future = Executors.newSingleThreadExecutor().submit(new Callable<InetAddress>() {
+                @Override
+                public InetAddress call() {
+                    try {
+                        return InetAddress.getByName("google.com");
+                    } catch (UnknownHostException e) {
+                        return null;
+                    }
+                }
+            });
+            inetAddress = future.get(timeOut, TimeUnit.MILLISECONDS);
+            future.cancel(true);
+        } catch (InterruptedException e) {
+        } catch (ExecutionException e) {
+        } catch (TimeoutException e) {
+        }
+        return inetAddress != null && !inetAddress.equals("");
+    }
+}
